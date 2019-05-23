5ad6cb4b2ac33b74ace53ead947c940a3ec7abff
Goutam Shepur <goutam.s.h@gmail.com>
2019-05-23 23:11:18 +0100

    Algorithms

--- src/test/java/com/gshepur/sorting/heapsort/HeapSortTest.java
+++ src/test/java/com/gshepur/sorting/heapsort/HeapSortTest.java
@@ -47,4 +47,56 @@ public class HeapSortTest {
         Arrays.sort(expectedArray);
         Assert.assertArrayEquals(expectedArray, array);
     }
+
+    @Test
+    public void sort4() {
+        HeapSort heapSort = new HeapSort();
+        int[] array = new int[]{1,1,8,8,2,2,1};
+        System.out.println("Before array sorted : "+Arrays.toString(array));
+        heapSort.sort(array);
+        System.out.println("After array sorted : "+Arrays.toString(array));
+
+        int[] expectedArray = new int[]{1,1,8,8,2,2,1};
+        Arrays.sort(expectedArray);
+        Assert.assertArrayEquals(expectedArray, array);
+    }
+
+    @Test
+    public void sort5() {
+        HeapSort heapSort = new HeapSort();
+        int[] array = new int[]{1,1,8,8,2,2};
+        System.out.println("Before array sorted : "+Arrays.toString(array));
+        heapSort.sort(array);
+        System.out.println("After array sorted : "+Arrays.toString(array));
+
+        int[] expectedArray = new int[]{1,1,8,8,2,2};
+        Arrays.sort(expectedArray);
+        Assert.assertArrayEquals(expectedArray, array);
+    }
+
+    @Test
+    public void sort6() {
+        HeapSort heapSort = new HeapSort();
+        int[] array = new int[]{1,1,8,8,2};
+        System.out.println("Before array sorted : "+Arrays.toString(array));
+        heapSort.sort(array);
+        System.out.println("After array sorted : "+Arrays.toString(array));
+
+        int[] expectedArray = new int[]{1,1,8,8,2};
+        Arrays.sort(expectedArray);
+        Assert.assertArrayEquals(expectedArray, array);
+    }
+
+    @Test
+    public void sort7() {
+        HeapSort heapSort = new HeapSort();
+        int[] array = new int[]{1,6,8,2};
+        System.out.println("Before array sorted : "+Arrays.toString(array));
+        heapSort.sort(array);
+        System.out.println("After array sorted : "+Arrays.toString(array));
+
+        int[] expectedArray = new int[]{1,6,8,2};
+        Arrays.sort(expectedArray);
+        Assert.assertArrayEquals(expectedArray, array);
+    }
 }
